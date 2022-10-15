be1437478b9a99eaadac0a846a2ad4c3e6958422
afanzott <fanzott@parkside.at>
2022-10-15 16:24:16 +0200

    Update predict module

--- predict.py
+++ predict.py
@@ -15,20 +15,18 @@ def make_prediction(input_data: pd.DataFrame) -> dict:
     data = preproc_predict(input_data)
     target_pipeline = joblib.load(config["predict_pipeline"])
 
-    print(data)
-
     predictions = target_pipeline.predict(X=data)
     results = [np.exp(pred) for pred in predictions]
 
     return results
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     # add arguments for data file serving via terminal
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--data_file", required=True)
 
     args = parser.parse_args()
-    data_file = args.file
+    data_file = args.data_file
 
     print(make_prediction(input_data=data_file))
