a6e73c3519d3ac8ff7c9c3d60aa60e6ebb545afa
afanzott <fanzott@parkside.at>
2022-10-17 16:45:25 +0200

    Added model validation

--- train.py
+++ train.py
@@ -1,15 +1,19 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
 
 import joblib
 from datetime import datetime
+import argparse
+from model_validation import validation
 
 import pipeline as pipe
 from custom_preproc_classes.load_data import data_loading
 from custom_preproc_classes.config.core import config
 
 
-def training() -> None:
+def training(validation: bool) -> None:
     """Train the model."""
 
     # read training data
@@ -25,6 +29,7 @@ def training() -> None:
         random_state=config["random_state"],
     )
     y_train = np.log(y_train)
+    y_test = np.log(y_test)
 
     # fit model
     pipe.target_Val.fit(X_train, y_train)
@@ -33,6 +38,52 @@ def training() -> None:
     destination_file = config["pipeline_save_file_path"] + "_" + str(datetime.now())[:-7] + ".joblib"
     joblib.dump(pipe.target_Val, destination_file)
 
+    # persist train and test data
+    X_train.to_csv(config["folder_train_test_data"] + "X_train_" + str(datetime.now())[:-7] + ".csv", sep=";", header=True)
+    X_test.to_csv(config["folder_train_test_data"] + "X_test_" + str(datetime.now())[:-7] + ".csv", sep=";", header=True)
+
+    y_train.to_csv(config["folder_train_test_data"] + "y_train_" + str(datetime.now())[:-7] + ".csv", sep=";", header=True)
+    y_test.to_csv(config["folder_train_test_data"] + "y_test_" + str(datetime.now())[:-7] + ".csv", sep=";", header=True)
+
+    # model validation if passed by the user
+    if validation is True:
+        # determine mse, rmse and r2 on the training data
+        pred = pipe.target_Val.predict(X_train)
+
+        print('train mse: {}'.format(int(
+            mean_squared_error(np.exp(y_train), np.exp(pred)))))
+        print('train rmse: {}'.format(int(
+            mean_squared_error(np.exp(y_train), np.exp(pred), squared=False))))
+        print('train r2: {}'.format(
+            r2_score(np.exp(y_train), np.exp(pred))))
+        print()
+
+        # make predictions for test set
+        pred = pipe.target_Val.predict(X_test)
+
+        # determine mse, rmse and r2 on the test data
+        print('test mse: {}'.format(int(
+            mean_squared_error(np.exp(y_test), np.exp(pred)))))
+        print('test rmse: {}'.format(int(
+            mean_squared_error(np.exp(y_test), np.exp(pred), squared=False))))
+        print('test r2: {}'.format(
+            r2_score(np.exp(y_test), np.exp(pred))))
+        print()
+
+        # show real value vs. predicted as plot
+        plt.scatter(y_test, pred)
+        plt.xlabel('True target value')
+        plt.ylabel('Predicted target value')
+        plt.title('Evaluation of GBM Predictions')
+        plt.show()
+
 
 if __name__ == "__main__":
-    training()
+    # add argument if model validation should be done
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--validation", required=True, type=bool)
+
+    args = parser.parse_args()
+    validation = args.validation
+
+    training(validation=validation)
