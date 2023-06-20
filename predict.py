7043536b20f22ed713f923d9331493686932416f
pdanninger <danninger@parkside.at>
2023-06-20 16:38:15 +0200

    Update prediciton for mlflow capabilities; creation of test inputs for prediction

--- predict.py
+++ predict.py
@@ -5,9 +5,12 @@ from custom_preproc_classes.load_data import data_loading_pred
 from custom_preproc_classes.config.core import config
 import logs
 
-import joblib
 import argparse
 import os
+import mlflow
+
+
+
 
 
 def make_prediction(input_data: str) -> dict:
@@ -30,7 +33,12 @@ def make_prediction(input_data: str) -> dict:
 
     # =========== LOAD PIPELINE ==========
     try:
-        target_pipeline = joblib.load(config["predict_pipeline"])
+        mlflow.set_tracking_uri("sqlite:///mlruns.db")
+        model_name = "Best Practice"
+        stage = "Production"
+        target_pipeline = mlflow.sklearn.load_model(
+            model_uri=f"models:/{model_name}/{stage}"
+        )
         log.info("Pipeline loaded successfully.")
     except Exception as e:
         log.error("Error loading pipeline", exc_info=e)
